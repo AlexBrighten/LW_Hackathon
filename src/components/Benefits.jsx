316dad5c6201ed2044285bd82df69803edef0655
AlexBrighten <alexjob3606@gmail.com>
2024-10-25 13:57:16 +0530

    Customized Hero section

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -8,10 +8,10 @@ import ClipPath from "../assets/svg/ClipPath";
 const Benefits = () => {
   return (
     <Section id="features">
-      <div className="container relative z-2">
+      <div className="mt-28 container relative z-2">
         <Heading
           className="md:max-w-md lg:max-w-2xl"
-          title="Chat Smarter, Not Harder with Brainwave"
+          title="Themes"
         />
 
         <div className="flex flex-wrap gap-10 mb-10">
