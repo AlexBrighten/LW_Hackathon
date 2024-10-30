832e54539477f18c32b73693df22af3714f6eda4
Mano <112639423+Manavalan2517@users.noreply.github.com>
2024-10-30 12:42:39 +0530

    fixed_bug: navigation_links

--- app/page.tsx
+++ app/page.tsx
@@ -15,8 +15,8 @@ import { FloatingNav } from "@/components/ui/FloatingNavbar";
 
 const Home = () => {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 pt-1">
+      <div className="max-w-7xl w-full pt-1">
         <FloatingNav navItems={navItems} />
         <Hero />
         <About />
